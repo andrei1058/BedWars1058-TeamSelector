c149a1cc270dd8fbf59dc77e215cee02b420de33
andrei1058 <andrew.dascalu@gmail.com>
2018-10-18 11:44:31 +0200

    0.3.2 release

---
+++ README.md
@@ -0,0 +1,15 @@
+##### BedWars1058-TeamSelector
+
+This is an addon plugin for BedWars1058. This plug-in will give you an item when you join an arena. You can use it to choose in which team to play.
+
+![ss1](https://image.ibb.co/eaJyvK/Screenshot_1.png)
+
+
+---
+**Server owner documentation**
+* [Configuration](config)
+* [Messages](messages)
+
+**Developer documentation**
+* [Getting started](api)
+* [JavaDoc](http://javadocs.andrei1058.com/BedWars1058-TeamSelector/)
---
+++ src/main/java/com/andrei1058/bedwars/teamselector/listeners/InventoryListener.java
@@ -0,0 +1,47 @@
+package com.andrei1058.bedwars.teamselector.listeners;
+
+import com.andrei1058.bedwars.Main;
+import com.andrei1058.bedwars.teamselector.configuration.Config;
+import com.andrei1058.bedwars.teamselector.teamselector.TeamSelectorGUI;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class InventoryListener implements Listener {
+
+    @EventHandler
+    //Prevent inventory move
+    public void onInventoryClick(InventoryClickEvent e){
+        ItemStack i = e.getCurrentItem();
+        if (i == null) return;
+        if (i.getType() == Material.AIR) return;
+        if (TeamSelectorGUI.openGUIs.contains(e.getWhoClicked().getUniqueId())) e.setCancelled(true);
+        if (!Main.nms.isCustomBedWarsItem(i)) return;
+        String identifier = Main.nms.getCustomData(i);
+
+        if (identifier.equals(TeamSelectorGUI.TEAM_SELECTOR_IDENTIFIER)){
+            e.setCancelled(Config.config.getBoolean(Config.ALLOW_MOVE_TROUGH_INVENTORY));
+        } else if (identifier.startsWith(TeamSelectorGUI.TEAM_JOIN_IDENTIFIER)){
+            String[] s = identifier.split("_");
+            if (s.length == 2){
+                if (TeamSelectorGUI.joinTeam((Player) e.getWhoClicked(), s[1])){
+                    Config.playSound((Player) e.getWhoClicked(), Config.SUCCESS_SOUND);
+                } else {
+                    Config.playSound((Player) e.getWhoClicked(), Config.ERROR_SOUND);
+                }
+                e.getWhoClicked().closeInventory();
+            }
+        }
+    }
+
+    @EventHandler
+    public void onInvClose(InventoryCloseEvent e){
+        if (TeamSelectorGUI.openGUIs.contains(e.getPlayer().getUniqueId())){
+            TeamSelectorGUI.openGUIs.remove(e.getPlayer().getUniqueId());
+        }
+    }
+}
